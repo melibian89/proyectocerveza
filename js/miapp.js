5eac34f0db40efd89fab11021ea72a6e75bf29a0
melibian89 <55601030+melibian89@users.noreply.github.com>
2022-11-21 23:50:48 -0300

    update de local storage

--- js/miapp.js
+++ js/miapp.js
@@ -14,6 +14,7 @@ const DOMitems = document.querySelector('#items');
 const DOMcarrito = document.querySelector('#carrito');
 const DOMtotal = document.querySelector('#total');
 const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+const miLocalStorage = window.localStorage;
 
 // Funciones
 function renderizarProductos() {
